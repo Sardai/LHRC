ec94c77ef26bece4b6f59998dad0bfec71b72316
Chris <chris_134a@hotmail.com>
2015-05-19 13:34:08 +0200

    push test

--- TJOONER/src/lhrc/group3/tjooner/MainActivity.java
+++ TJOONER/src/lhrc/group3/tjooner/MainActivity.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+/**
+ * 
+ * @author Chris
+ *
+ */
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);       
     }
 
 
