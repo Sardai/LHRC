a4a34a0e32b479759214e9a4bb39497bc35b93fb
Luuk Wellink <luuk-wellink@hotmail.com>
2015-05-21 12:54:44 +0200

    Camera functie opstarten, aanpassingen in: AndroidManifest, activity_main.xml, MainActivity

--- TJOONER/src/lhrc/group3/tjooner/MainActivity.java
+++ TJOONER/src/lhrc/group3/tjooner/MainActivity.java
@@ -1,42 +1,61 @@
 package lhrc.group3.tjooner;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * @author Chris Rötter, Luuk Wellink
  * @author Hugo van der Geest, Rene Bisperink
  * 
- *
+ * 
  */
 public class MainActivity extends Activity {
+	private Button ButtonCamera;
+	public static int TAKE_PICTURE = 1;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        setContentView(R.layout.activity_main);
-    }
+		ButtonCamera = (Button) findViewById(R.id.buttonCamera);
 
+		ButtonCamera.setOnClickListener(new View.OnClickListener() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+				// start camera activity
+				startActivityForResult(intent, TAKE_PICTURE);
+
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
